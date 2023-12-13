89865888016051ab6c3ef8432c82b7cc485eaa85
milesChild <colechild1@gmail.com>
2023-12-13 00:30:11 -0500

    update redis client to work with trades

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,9 @@
 #[allow(unused_imports)]
 
 use kalshi_wss::Snapshot;
+use kalshi_wss::Trade;
 use redis_utils::RedisOrderbookClient;
+use clap::{App, Arg};
 
 pub mod redis_utils;
 pub mod kalshi_wss;
@@ -9,11 +11,28 @@ pub mod constants;
 
 fn main() -> Result<(), anyhow::Error> {
 
+    let args = Vec<String> = std::env::args().collect();
+
+    /// Check if user has provided the correct number of arguments
+    if args.len() != 3 {
+        eprintln!("Error: Please provide a data type flag (--snapshot, --ticker) and ticker.");
+        std::process::exit(1);
+    }
+
+    let mode = &args[1];
+    let ticker = &args[2];
+
     let mut redis_client = RedisOrderbookClient::new("redis://127.0.0.1")?;
-    let ticker = std::env::args().nth(1).expect("No ticker provided.");
 
-    let snap: Snapshot = redis_client.read(&ticker).expect("No data found for ticker.");
+    if mode == "--snapshot" {
+        let snap: Snapshot = redis_client.read_snapshot(&ticker).expect("No snapshot data found for ticker.");
+        println!("{snap:?}");
+    } else if mode == "--trade" {
+        let trade: Trade = redis_client.read_trade(&ticker).expect("No trade data found for ticker.");
+        println!("{trade:?}");
+    } else {
+        println!("Invalid type provided. Please select either --snapshot or --trade.");
+    }
 
-    println!("{snap:?}");
     Ok(())
 }
